94f923fa4a7152ebe3cac85d5582b9caecc467a1
Nesamani Ramakrishnan <81634732+NesaNesamani@users.noreply.github.com>
2022-01-31 18:21:46 +0530

    ROOT DIRECTORY

--- js/pages/project.js
+++ js/pages/project.js
@@ -58,8 +58,8 @@ $( "#floorplanform" ).validate({
     
     let fordata = { floorplan_yourname,floorplan_emailaddress,floorplan_mobilenumber,floorplan_description,type };
     
-    let bhk2 = ['../../sv/asset/2BHK.zip'];
-    let bhk3 = ['../../sv/asset/3BHK.zip'];
+    let bhk2 = ['../../asset/2BHK.zip'];
+    let bhk3 = ['../../asset/3BHK.zip'];
 
     //Button load
     document.getElementById("floorplan_submitbtn").disabled = true;
@@ -154,7 +154,7 @@ submitHandler: function(form){
   document.getElementById("broucher_submitbtn").innerHTML = "Loading ..."; 
   $.get("backend/brouchermail.php",fordata,function(data, status){
     //After mail sent
-    let filepath = '../../sv/asset/brochure.pdf';
+    let filepath = '../../asset/brochure.pdf';
     let link = document.createElement('a');
     link.href = filepath;
     link.download = "brochure.pdf";
